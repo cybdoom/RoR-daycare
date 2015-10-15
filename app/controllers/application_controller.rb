b350ff190d9cfd69eb932e1b5e4945f75ec18956
Shoaib Malik <shoaib2109@gmail.com>
2015-10-15 23:54:41 +0530

    edit todo page integrated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
-  before_action :protect_from_devise_signin
+  # before_action :protect_from_devise_signin
 
   def protect_from_devise_signin
     return unless request.get? 
