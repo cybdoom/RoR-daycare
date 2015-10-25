8ffa30bfeaff8a86794e7aebc72b6f4271ed48a4
Shoaib Malik <shoaib2109@gmail.com>
2015-10-26 01:05:52 +0530

    refactored todo job

--- app/jobs/logger_job.rb
+++ app/jobs/logger_job.rb
@@ -1,10 +1,26 @@
 class LoggerJob 
   include SuckerPunch::Job
-  
-  # cron_job :set_occurence, interval: 1.minute
 
   def perform(data)
     puts data
+  end
+
+  def set_occurence
+    todos = Todo.includes(:occurrences).where("todos.frequency != ? AND occurrences.schedule_date <= ? AND occurrences.due_date > ?", "One Time Event", DateTime.now, DateTime.now).references(:occurrences)
+    todos.each do |todo|
+
+      current_occurrence = todo.current_occurrence
+      unless todo.next_occurrences.present?
+        nxt_o = Occurrence.create(todo_id: todo.id, schedule_date: current_occurrence.next_schedule_date, due_date: current_occurrence.next_due_date, status: :draft)
+        if nxt_o.present? && nxt_o.valid?
+          nxt_o.save_user_occurrences(user_ids=todo.users.pluck(:id)) unless todo.is_circulatable?
+          puts("Next Occurrence Created Successfully")
+        else
+          puts("Failed to create Next Occurrence")
+          puts(nxt_o.errors.full_messages.join(", "))
+        end
+      end
+    end
   end
 
   def later(sec, data)
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -134,21 +134,7 @@ class Todo < ActiveRecord::Base
   end
   
   def self.set_todo_next_occurrence
-    todos = Todo.includes(:occurrences).where("todos.frequency != ? AND occurrences.schedule_date <= ? AND occurrences.due_date > ?", "One Time Event", DateTime.now, DateTime.now).references(:occurrences)
-    todos.each do |todo|
-
-      current_occurrence = todo.current_occurrence
-      unless todo.next_occurrences.present?
-        nxt_o = Occurrence.create(todo_id: todo.id, schedule_date: current_occurrence.next_schedule_date, due_date: current_occurrence.next_due_date, status: :draft)
-        if nxt_o.present? && nxt_o.valid?
-          nxt_o.save_user_occurrences(user_ids=todo.users.pluck(:id)) unless todo.is_circulatable?
-          puts("Next Occurrence Created Successfully")
-        else
-          puts("Failed to create Next Occurrence")
-          puts(nxt_o.errors.full_messages.join(", "))
-        end
-      end
-    end
+    LoggerJob.new.async.set_occurence
     logger.info "==============Cron Job Run At #{DateTime.now}==================="
   end
 
